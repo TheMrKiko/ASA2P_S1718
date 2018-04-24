bdf58c10bd5c9dc2a28835f064ac290a820b9078
João Daniel Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2018-04-24 11:54:50 +0100

    input de primeiro plano; known bug: falha para pesos maiores que 9

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 /********************************** GRUPO 154 **********************************/
 /*******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 /********************************* ESTRUTURAS *********************************/
@@ -16,32 +17,53 @@ using namespace std;
 
 /************************************ MAIN ************************************/
 int main(int argc, char const* argv[]) {
-    int altura, largura;
-
+    int altura, largura, i=0, j=0, arrayIndex = 0;
+    int* plano;
+    char c;
     // LER INPUT
     // tamanhos iniciais das tabelas
-    if (!scanf("%d %d", &altura, &largura) || altura =< 0 || largura =< 0 ) {
+    if (!scanf("%d %d", &altura, &largura) || altura <= 0 || largura <= 0 ) {
 		printf("Deu erro a ler\n");
 	}
 
-    getchar();
-
     // pesos do primeiro plano
     //TODO
-    getchar();
+    //getchar();
+    plano = new int[altura*largura];
+    while(i<altura) {
+        getchar();
+        //printf("NOVA LINHA %d\n", i);
+        j = 0;
+        while (j<largura) {
+            //printf("NOVA COLUNA %d\n", j);
+            c = getchar();
+            if('0'<= c && c<='9') {
+                // printf("char %c\n", c);
+                plano[arrayIndex++] = atoi(&c);
+                j++;
+            }
+        }
+        i++;
+    }
+    printf("plano:\n");
+    for(i=0;i<altura*largura;i++) {
+        printf("%d", plano[i]);
+    }
+    printf("\n------\n");
+
 
     // pesos do cenario
     //TODO
-    getchar();
+    //getchar();
 
     // pesos das vizinhancas horizontais
     // TODO
-    getchar();
+    //getchar();
 
     // pesos das vizinhancas verticais
     // TODO
 
-    
+
 	return 0;
 }
 
