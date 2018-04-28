3d93e2479f8abc3c0fdc90d718cdc7d4bbd3fb17
Joao Daniel Rua Ferreira Pires da Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2018-04-28 17:43:13 +0100

    pusha da source para o target closes #6

--- main.cpp
+++ main.cpp
@@ -27,32 +27,33 @@ typedef struct vertice {
 } Vertice;
 
 /********************************* PROTOTIPOS *********************************/
+void printVertices(Vertice* verts, int);
 
 /************************************ MAIN ************************************/
 int main(int argc, char const* argv[]) {
-    int numLin, numCol, numV, aux, l, c, v;
-	Vertice* vertices;
+    int numLin, numCol, numV, aux, l, c, v, min;
+	Vertice* verts;
 
-    // LER INPUT
+    // LER INPUT ---------------------------------------------------------------
     // tamanhos iniciais das tabelas -------------
     if (!scanf("%d %d", &numLin, &numCol) || numLin <= 0 || numCol <= 0 ) {
 		printf("Deu erro a ler\n");
 	}
 
 	numV = numLin * numCol;
-	vertices = new vertice[numV + 1];
+	verts = new vertice[numV + 1];
 
     // pesos do primeiro plano -------------
 	// getchar();
 
 	for (v = 0; v < numV; v++) {
-		scanf("%d", &vertices[v].fsource[CAPACITY]);
-		vertices[v].fsource[FLOW] = 0;
+		scanf("%d", &verts[v].fsource[CAPACITY]);
+		verts[v].fsource[FLOW] = 0;
 	}
 
     /*printf("plano:\n");
     for (v = 0; v < numV; v++) {
-        printf("%d/%d-", vertices[v].fsource[FLOW], vertices[v].fsource[CAPACITY]);
+        printf("%d/%d-", verts[v].fsource[FLOW], verts[v].fsource[CAPACITY]);
     }
     printf("\n------\n");*/
 
@@ -60,11 +61,11 @@ int main(int argc, char const* argv[]) {
 	// getchar();
 
 	for (v = 0; v < numV; v++) {
-		scanf("%d", &vertices[v].target[CAPACITY]);
-		vertices[v].target[FLOW] = 0;
+		scanf("%d", &verts[v].target[CAPACITY]);
+		verts[v].target[FLOW] = 0;
 	}
     /*for (v = 0; v < numV; v++) {
-        printf("%d-%d", vertices[v].target[FLOW], vertices[v].target[CAPACITY]);
+        printf("%d-%d", verts[v].target[FLOW], verts[v].target[CAPACITY]);
     }
     printf("\n------\n");*/
 
@@ -73,26 +74,45 @@ int main(int argc, char const* argv[]) {
 	for (l = 0; l < numLin; l++) {
 		for (c = 0; c < numCol - 1; c++) {
 			scanf("%d", &aux);
-			vertices[pos(l, c)].east[CAPACITY] = aux;
-			vertices[pos(l, c)].east[FLOW] = 0;
-			vertices[pos(l, c+1)].west[CAPACITY] = aux;
-			vertices[pos(l, c+1)].west[FLOW] = 0;
+			verts[pos(l, c)].east[CAPACITY] = aux;
+			verts[pos(l, c)].east[FLOW] = 0;
+			verts[pos(l, c+1)].west[CAPACITY] = aux;
+			verts[pos(l, c+1)].west[FLOW] = 0;
 		}
 	}
 
-    // pesos das vizinhancas verticais
+    // pesos das vizinhancas verticais -------------
 	// getchar();
 	for (l = 0; l < numLin - 1; l++) {
 		for (c = 0; c < numCol; c++) {
 			scanf("%d", &aux);
-			vertices[pos(l, c)].south[CAPACITY] = aux;
-			vertices[pos(l, c)].south[FLOW] = 0;
-			vertices[pos(l+1, c)].north[CAPACITY] = aux;
-			vertices[pos(l+1, c)].north[FLOW] = 0;
+			verts[pos(l, c)].south[CAPACITY] = aux;
+			verts[pos(l, c)].south[FLOW] = 0;
+			verts[pos(l+1, c)].north[CAPACITY] = aux;
+			verts[pos(l+1, c)].north[FLOW] = 0;
 		}
 	}
+
+	// PUSHEI PUSHANDO da source para a target toda ----------------------------
+	for (v = 0; v < numV; v++) {
+		min = min(verts[v].fsource[CAPACITY], verts[v].target[CAPACITY]);
+		verts[v].fsource[FLOW] = verts[v].target[FLOW] = min;
+	}
+	printVertices(verts, numV);
 
 	return 0;
 }
 
 /******************************* OUTRAS FUNCOES *******************************/
+void printVertices(Vertice* verts, int numV) {
+	int v;
+	for (v = 0; v < numV; v++) {
+		printf("V: %d - %d/%d | n%d/%d s%d/%d e%d/%d o%d/%d | %d/%d\n", v,
+		verts[v].fsource[FLOW], verts[v].fsource[CAPACITY],
+		verts[v].north[FLOW], verts[v].north[CAPACITY],
+		verts[v].south[FLOW], verts[v].south[CAPACITY],
+		verts[v].east[FLOW], verts[v].east[CAPACITY],
+		verts[v].west[FLOW], verts[v].west[CAPACITY],
+		verts[v].target[FLOW], verts[v].target[CAPACITY]);
+	}
+}
