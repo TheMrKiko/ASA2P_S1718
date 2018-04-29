0a57443cb83bafe38eeb40c0c5df50fa8d3d7d98
Joao Daniel Rua Ferreira Pires da Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2018-04-29 18:06:31 +0100

    Achamos que faz muita coisa mas n

--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@
 /*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <stack>
+#include <queue>
 using namespace std;
 
 #define min(A, B) A < B ? A : B
 #define FLOW 0
 #define CAPACITY 1
+#define SOURCE 'f'
+#define NORTH 'n'
+#define SOUTH 's'
+#define EAST 'd'
+#define WEST 'e'
+#define TARGET 't'
+#define INVALID -1
+#define SOURCE_ID -2
+#define TARGET_ID numCol * numLin
 #define pos(line, column) line * numCol + column
 
 /********************************* ESTRUTURAS *********************************/
@@ -29,6 +38,12 @@ typedef struct vertice {
 
 /********************************* PROTOTIPOS *********************************/
 void printVertices(Vertice verts, int);
+int Edmondzinho(Vertice verts, int numLin, int numCol);
+int getDiffFromTo(int from, int to, int numCol, int numLin, Vertice verts);
+void setDiffFromTo(int from, int to, int numCol, int numLin, Vertice verts, int df);
+char relatedPos(int from, int to, int numCol, int numLin);
+int getVOfDir(int v, char dir, int numCol, int numLin);
+int bfs(Vertice verts, int* pi, int numCol, int numLin);
 
 /************************************ MAIN ************************************/
 int main(int argc, char const* argv[]) {
@@ -100,6 +115,10 @@ int main(int argc, char const* argv[]) {
 		verts[v].fsource[FLOW] = verts[v].target[FLOW] = min;
 	}
 	printVertices(verts, numV);
+	// ALGORITMANDO ------------------------------------------------------------
+	Edmondzinho(verts, numLin, numCol);
+
+	printVertices(verts, numV);
 
 	return 0;
 }
@@ -119,69 +138,142 @@ void printVertices(Vertice verts, int numV) {
 }
 
 
-void Edmondzinho(Vertice verts, int numV, int numCol) {
-	int i, flow = 0, targ = numV, pi[numV], foundPath = 1, df;
+int Edmondzinho(Vertice verts, int numLin, int numCol) {
+	int v, flow = 0, numV = numCol * numLin, pi[numV + 1], df;
 
-	for (i=0; i<numV; i++) {
-		pi[i] = -1;
-	}
-	do {
-		foundPath = bfs(verts, pi);
-	} while (foundPath) {
-
-		df = verts[pi[targ]].target[CAPACITY] - verts[pi[targ]].target[FLOW];
-		for (v = pi[targ]; v != -1; v = pi[v]) {//PODE-SE POUPAR AQUI IF NEED
-			df = min(df, getDiffFromTo(p[v], v,  numCol));
+	while (bfs(verts, pi, numCol, numLin)) {
+		df = verts[pi[TARGET_ID]].target[CAPACITY] - verts[pi[TARGET_ID]].target[FLOW];
+		for (v = pi[TARGET_ID]; v != -1; v = pi[v]) {//PODE-SE POUPAR AQUI IF NEED
+			df = min(df, getDiffFromTo(pi[v], v,  numCol, numLin, verts));
 		}
-
+		for (v = pi[TARGET_ID]; v != -1; v = pi[v]) {
+			setDiffFromTo(pi[v], v,  numCol, numLin, verts, df);
+		}
+		flow += df;
 	}
-
+	return flow;
 }
 
-int getDiffFromTo(int from, int to, int numCol, Vertice verts) {
-	switch (relatedPos(from, to, numCol)) {
-		case 'd':
-			return verts[from].east[CAPACITY] - verts[to].east[FLOW];
-			break;
-		case 'e':
-			return verts[from].west[CAPACITY] - verts[to].west[FLOW];
-			break;
-		case 's':
-			return verts[from].south[CAPACITY] - verts[to].south[FLOW];
-			break;
-		case 'n':
-			return verts[from].north[CAPACITY] - verts[to].north[FLOW];
-			break;
-	}
-}
 
-void setDiffFromTo(int from, int to, int numCol, Vertice verts) {
-	switch (relatedPos(from, to, numCol)) {
-		case 'd':
-			verts[from].east[CAPACITY] - verts[to].east[FLOW];
+int getDiffFromTo(int from, int to, int numCol, int numLin, Vertice verts) {
+	switch (relatedPos(from, to, numCol, numLin)) {
+		case SOURCE:
+			return verts[to].fsource[CAPACITY] - verts[to].fsource[FLOW];
 			break;
-		case 'e':
-			verts[from].west[CAPACITY] - verts[to].west[FLOW];
+		case EAST:
+			return verts[from].east[CAPACITY] - verts[from].east[FLOW];
 			break;
-		case 's':
-			verts[from].south[CAPACITY] - verts[to].south[FLOW];
+		case WEST:
+			return verts[from].west[CAPACITY] - verts[from].west[FLOW];
 			break;
-		case 'n':
-			verts[from].north[CAPACITY] - verts[to].north[FLOW];
+		case SOUTH:
+			return verts[from].south[CAPACITY] - verts[from].south[FLOW];
+			break;
+		case NORTH:
+			return verts[from].north[CAPACITY] - verts[from].north[FLOW];
+			break;
+		case TARGET:
+			return verts[from].target[CAPACITY] - verts[from].target[FLOW];
+			break;
+		default:
+			return INVALID;
 			break;
 	}
 }
 
-char relatedPos(int from, int to, int numCol) {
-	if (to - from == 1 ) {
-		return 'd'
-	} else if (to - from == -1) {
-		return 'e';
+void setDiffFromTo(int from, int to, int numCol, int numLin, Vertice verts, int df) {
+	switch (relatedPos(from, to, numCol, numLin)) {
+		case SOURCE:
+			verts[to].fsource[FLOW] += df;
+			break;
+		case EAST:
+			verts[from].east[FLOW] += df;
+			verts[to].west[FLOW] -= df;
+			break;
+		case WEST:
+			verts[from].west[FLOW] += df;
+			verts[to].east[FLOW] -= df;
+			break;
+		case SOUTH:
+			verts[from].south[FLOW] += df;
+			verts[to].north[FLOW] -= df;
+			break;
+		case NORTH:
+			verts[from].north[FLOW] += df;
+			verts[to].south[FLOW] -= df;
+			break;
+		case TARGET:
+			verts[from].target[FLOW] += df;
+			break;
+		default:
+			break;
+	}
+}
+
+char relatedPos(int from, int to, int numCol, int numLin) {
+	if (from == SOURCE_ID) {
+		return SOURCE;
+	} else if (to == TARGET_ID) {
+		return TARGET;
+	} else if (to - 1 == from) {
+		return EAST;
+	} else if (to + 1 == from) {
+		return WEST;
 	} else if ( (to - from) > 0 && (to - from) % numCol == 0 ) {
-		return 's';
+		return SOUTH;
 	} else {
-		return 'n';
+		return NORTH;
 	}
 }
 
-void joaochupaila()
+int getVOfDir(int v, char dir, int numCol, int numLin) {
+	switch (dir) {
+		case EAST:
+			return (v + 1) % numCol ? -1 : v + 1;
+			break;
+		case WEST:
+			return v % numCol ? -1 : v - 1;
+			break;
+		case SOUTH:
+			return numCol * (numLin - 1) <= v ? -1 : v + numCol;
+			break;
+		case NORTH:
+			return v < numCol ? -1 : v - numCol;
+			break;
+		case TARGET:
+			return TARGET_ID;
+			break;
+		default:
+			return INVALID;
+			break;
+	}
+}
+
+
+int bfs(Vertice verts, int* pi, int numCol, int numLin) {
+	queue<int> queueV;
+	int v, d, nextV, numV = numCol * numLin;
+	char dirs[5] = {NORTH, SOUTH, EAST, WEST, TARGET};
+
+	for(v = 0; v<numV; v++) {
+		if (getDiffFromTo(SOURCE_ID, v, numCol, numLin, verts)) {
+			queueV.push(v);
+			pi[v] = SOURCE_ID;
+		} else {
+			pi[v] = INVALID;
+		}
+	}
+
+	while (queueV.size()) {
+		v = queueV.front();
+		queueV.pop();
+		for(d = 0; d<5; d++) {
+			nextV = getVOfDir(v, dirs[d], numCol, numLin);
+			if (nextV != INVALID && pi[nextV] == INVALID && getDiffFromTo(v, nextV, numCol, numLin, verts)) {
+				queueV.push(nextV);
+				pi[nextV] = v;
+			}
+		}
+	}
+	return pi[TARGET_ID] != INVALID;
+}
